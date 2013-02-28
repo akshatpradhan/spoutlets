d50714bc3fa8b8641e13bef5dd8d383ddbf9f170
Akshat Pradhan <akshat.ip@gmail.com>
2013-02-28 18:30:56 -0500

    improve profile layout and add delete functionality

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -79,7 +79,7 @@ class EntriesController < ApplicationController
     @entry.destroy
 
     respond_to do |format|
-      format.html { redirect_to entries_url }
+      format.html { redirect_to user_url(current_user), notice: 'Entry was successfully deleted.' }
       format.json { head :no_content }
     end
   end
