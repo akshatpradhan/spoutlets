25cd242f884a903e1a28c254c251e9ea1f217752
Akshat Pradhan <akshat.ip@gmail.com>
2013-03-17 14:14:56 -0400

    remove ability for world to see /users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :correct_user?, :except => [:index]
-
+  before_filter :correct_user?, except: [:index, :show]
+  load_and_authorize_resource except: [:show, :edit]
   def index
     @users = User.all
   end
