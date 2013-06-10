11fb7872bd7b4fe8130233355ad6078a4893ea4c
Andrea D'Amico <andrea.damico@gmail.com>
2013-06-10 12:36:07 +0200

    fixes entries controller tests

--- spec/controllers/entries_controller_spec.rb
+++ spec/controllers/entries_controller_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe EntriesController do
+  let(:entry) { FactoryGirl.build(:entry) }
+  let(:valid_attributes) { entry.attributes }
+  let(:user) { FactoryGirl.create(:user) }
+
+  before do
+    session[:user_id] = user.id
+  end
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
@@ -14,8 +21,8 @@ describe EntriesController do
   # end
 
   describe "GET index" do
-    let(:entry) { FactoryGirl.create(:entry) }
     it "returns the entries list" do
+      entry.save!
       get :index
       assigns(:entries).should eq([entry])
     end
@@ -23,15 +30,15 @@ describe EntriesController do
 
   describe "GET new" do
     it "assigns a new entry as @entry" do
-      get :new, {}, valid_session
+      get :new, {}
       assigns(:entry).should be_a_new(Entry)
     end
   end
 
   describe "GET edit" do
-    let(:entry) { FactoryGirl.create(:entry) }
     it "assigns the requested entry as @entry" do
-      get :edit, {:id => entry.to_param}, valid_session
+      entry.save!
+      get :edit, {:id => entry.to_param}
       assigns(:entry).should eq(entry)
     end
   end
@@ -40,19 +47,19 @@ describe EntriesController do
     describe "with valid params" do
       it "creates a new Entry" do
         expect {
-          post :create, {:entry => valid_attributes}, valid_session
+          post :create, {:entry => valid_attributes}
         }.to change(Entry, :count).by(1)
       end
 
       it "assigns a newly created entry as @entry" do
-        post :create, {:entry => valid_attributes}, valid_session
+        post :create, {:entry => valid_attributes}
         assigns(:entry).should be_a(Entry)
         assigns(:entry).should be_persisted
       end
 
-      it "redirects to the created entry" do
-        post :create, {:entry => valid_attributes}, valid_session
-        response.should redirect_to(Entry.last)
+      it "redirects to the user profile path" do
+        post :create, {:entry => valid_attributes}
+        response.should redirect_to(user_url(user))
       end
     end
 
@@ -60,23 +67,25 @@ describe EntriesController do
       it "assigns a newly created but unsaved entry as @entry" do
         # Trigger the behavior that occurs when invalid params are submitted
         Entry.any_instance.stub(:save).and_return(false)
-        post :create, {:entry => { "content" => "invalid value" }}, valid_session
+        post :create, {:entry => { "content" => "invalid value" }}
         assigns(:entry).should be_a_new(Entry)
       end
 
       it "re-renders the 'new' template" do
         # Trigger the behavior that occurs when invalid params are submitted
         Entry.any_instance.stub(:save).and_return(false)
-        post :create, {:entry => { "content" => "invalid value" }}, valid_session
+        post :create, {:entry => { "content" => "invalid value" }}
         response.should render_template("new")
       end
     end
   end
 
   describe "PUT update" do
+    before do
+      entry.save!
+    end
+
     describe "with valid params" do
-      let(:entry) { FactoryGirl.create(:entry) }
-      let(:valid_attributes) { entry.attributes }
       it "updates the requested entry" do
 
         # Assuming there are no other entries in the database, this
@@ -84,53 +93,52 @@ describe EntriesController do
         # receives the :update_attributes message with whatever params are
         # submitted in the request.
         Entry.any_instance.should_receive(:update_attributes).with({ "content" => "" })
-        put :update, {:id => entry.to_param, :entry => { "content" => "" }}, valid_session
+        put :update, {:id => entry.to_param, :entry => { "content" => "" }}
       end
 
       it "assigns the requested entry as @entry" do
         user = FactoryGirl.create(:user)
         session[:session_id] = user.id
-        put :update, {:id => entry.to_param, :entry => valid_attributes}, valid_session
+        put :update, {:id => entry.to_param, :entry => valid_attributes}
         assigns(:entry).should eq(entry)
       end
 
-      it "redirects to the entry" do
-        put :update, {:id => entry.to_param, :entry => valid_attributes}, valid_session
-        response.should redirect_to(entry)
+      it "redirects to the user profile path" do
+        put :update, {:id => entry.to_param, :entry => valid_attributes}
+        response.should redirect_to(user_url(user))
       end
     end
 
     describe "with invalid params" do
       it "assigns the entry as @entry" do
-        entry = Entry.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
         Entry.any_instance.stub(:save).and_return(false)
-        put :update, {:id => entry.to_param, :entry => { "content" => "invalid value" }}, valid_session
+        put :update, {:id => entry.to_param, :entry => { "content" => "invalid value" }}
         assigns(:entry).should eq(entry)
       end
 
       it "re-renders the 'edit' template" do
-        entry = Entry.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
         Entry.any_instance.stub(:save).and_return(false)
-        put :update, {:id => entry.to_param, :entry => { "content" => "invalid value" }}, valid_session
+        put :update, {:id => entry.to_param, :entry => { "content" => "invalid value" }}
         response.should render_template("edit")
       end
     end
   end
 
   describe "DELETE destroy" do
+    before do
+      entry.save!
+    end
     it "destroys the requested entry" do
-      entry = Entry.create! valid_attributes
       expect {
-        delete :destroy, {:id => entry.to_param}, valid_session
+        delete :destroy, {:id => entry.to_param}
       }.to change(Entry, :count).by(-1)
     end
 
-    it "redirects to the entries list" do
-      entry = Entry.create! valid_attributes
-      delete :destroy, {:id => entry.to_param}, valid_session
-      response.should redirect_to(entries_url)
+    it "redirects to the user profile path" do
+      delete :destroy, {:id => entry.to_param}
+      response.should redirect_to(user_url(user))
     end
   end
 
