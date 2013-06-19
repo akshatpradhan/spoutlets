ac3de8ad4efaa59f6d3aa2c75be3b6054e974cfe
Akshat Pradhan <akshat.ip@gmail.com>
2013-06-19 16:07:11 -0400

    add high chart on entry model for landing page

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -4,4 +4,18 @@ describe Entry do
   it { should respond_to :user }
   it { should validate_presence_of :content }
   it { should validate_presence_of :category }
+
+  describe ".fetch_categories" do
+    before do
+      %w(dreams/sleep sex/relationships work/school).each do |c|
+        FactoryGirl.create(:entry, category: c)
+      end
+    end
+    it "should return an array of categories" do
+      Entry.all.fetch_categories.should == [["dreams/sleep", 1],
+                                            ["sex/relationships", 1],
+                                            ["work/school", 1]]
+    end
+  end
+
 end
