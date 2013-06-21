cbed20522aa69db4d347330a6ca52ca7eaf0a10a
Akshat Pradhan <akshat.ip@gmail.com>
2013-06-21 11:09:48 -0400

    add sessions controller tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class SessionsController < ApplicationController
 
   def new
@@ -7,7 +8,7 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    user = User.where(:provider => auth['provider'], 
+    user = User.where(:provider => auth['provider'],
                       :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
 ## Per http://guides.rubyonrails.org/security.html#sessions
 ## 2.8 Session Fixation – Countermeasure: reset the session after login:
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
     session[:user_id] = user.id
     user.add_role :admin if User.count == 1 # make the first user an admin
     if user.email.blank?
-      redirect_to edit_user_path(user), :alert => "Please enter your email address."
+      redirect_to edit_user_path(user), alert: "Logged in as #{user.name}. Please enter your email address."
     else
       redirect_to root_url, :notice => 'Signed in!'
     end
