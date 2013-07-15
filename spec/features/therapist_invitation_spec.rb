85c5f28f3e6145c77ef11799c644149fa51fc6f2
Akshat Pradhan <akshat.ip@gmail.com>
2013-07-15 18:12:40 -0400

    add: Therapist accept invitation and create account

--- spec/features/therapist_invitation_spec.rb
+++ spec/features/therapist_invitation_spec.rb
@@ -17,19 +17,31 @@ feature "User wanting to share entries with therapist" do
   end
 
   scenario "invites a therapist" do
-    # GET /therapists/invitation/new
     visit new_therapist_invitation_path
+    # NOTE => GET /therapists/invitation/new after signing in
     fill_in 'email', with: 'therapist@example.com'
     click_button 'Enable Access!'
     page.should have_content('An invitation email has been sent to therapist@example.com.')
     page.should have_content(/your journal/i)
-    # Invitation email should be sent with inviter's last 5 journal entries.
+    ActionMailer::Base.deliveries.last.to.should include("therapist@example.com")
+    # NOTE => Fake send an email after clicking button
+    # TODO Invitation email should be sent with inviter's last 5 journal entries.
+
   end
 end
 
 feature "Therapist wanting to access entries of inviter" do
   scenario "creates a new account" do
-    pending "figure out accept_invitation_path"
+    therapist = Therapist.invite!(email: "therapist@example.com")
+    therapist.accept_invitation!
+    visit accept_therapist_invitation_path(invitation_token: therapist.invitation_token)
+    # NOTE => GET /therapists/invitation/accept?invitation_token=uqASMfBxmfXvzSxg9q4b
+    fill_in 'Name', with: 'Deana Finkel'
+    fill_in 'Password', with: 'Password1'
+    fill_in 'Password confirmation', with: 'Password1'
+    click_button 'Create account!'
+    page.should have_content(/Your password was set successfully. You are now signed in./i)
+    page.should have_content(/Client Journals/i)
   end
 
   scenario "views entries of inviter" do
