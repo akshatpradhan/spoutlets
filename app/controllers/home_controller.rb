8785d5076e1f600e835c5c9d0497b7d3fc2e8dd1
Akshat Pradhan <akshat.ip@gmail.com>
2013-09-05 09:22:21 -0400

    add: landing page
    * restrict landing page to show only 3 entries
    * remove marketing copy from community

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @entries = Entry.where(published: true).desc(:created_at).limit(3)
 
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @entries }
+    end
   end
 end
