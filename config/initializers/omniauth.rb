f009e33a053dae8d1fa897204ff7d347a1c2f9b9
Andrea D'Amico <andrea.damico@gmail.com>
2013-09-27 13:43:55 +0200

    adding omniauth-linkedin and prepare devise inits

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
+  provider :linkedin, ENV['OMNIAUTH_LINKEDIN_KEY'], ENV['OMNIAUTH_LINKEDIN_SECRET']
 end
