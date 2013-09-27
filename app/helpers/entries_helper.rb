d4d976abd654797aeb8f6f0864f18fff7b9069f2
Andrea D'Amico <andrea.damico@gmail.com>
2013-09-27 11:25:16 +0200

    entry_status_for case for user not being a liker, specs for entry_button_for

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -8,17 +8,17 @@ module EntriesHelper
     return "No one else knows the feeling." if entry.likes == 0
     out = []
     if entry.likes == 1
-      if user == entry.user
-        out << "Another person knows"
-      else
+      if entry.liked?(user)
         out << "You know"
+      else
+        out << "Another person knows"
       end
     else#likes > 1
-      if user == entry.user
-        likes = entry.likes
-      else
+      if entry.liked?(user)
         likes = entry.likes - 1
         out << "You and"
+      else
+        likes = entry.likes
       end
       out << "<b>#{likes}</b> others know"
     end
@@ -27,15 +27,17 @@ module EntriesHelper
   end
 
   def like_button_for(entry, user)
-    if entry.liked?(user)
-      like_or_unlike_action = unlike_entry_path(entry)
-      css_class = "active"
-      title = " un-been there"
-    else
-      like_or_unlike_action = like_entry_path(entry)
-      css_class = ""
-      title = " been there"
+    unless user == entry.user
+      if entry.liked?(user)
+        like_or_unlike_action = unlike_entry_path(entry)
+        css_class = "active"
+        title = " un-been there"
+      else
+        like_or_unlike_action = like_entry_path(entry)
+        css_class = ""
+        title = " been there"
+      end
+      return link_to(content_tag(:i, nil, class: "icon-heart") + title, like_or_unlike_action, method: :put, remote: true, class: "btn btn-mini btn-inverse pull-right #{css_class}")
     end
-    return link_to(content_tag(:i, nil, class: "icon-heart") + title, like_or_unlike_action, method: :put, remote: true, class: "btn btn-mini btn-inverse pull-right #{css_class}")
   end
 end
--- spec/helpers/entries_helper_spec.rb
+++ spec/helpers/entries_helper_spec.rb
@@ -39,13 +39,18 @@ describe EntriesHelper do
             .should == "Another person knows the feeling."
         end
       end
-      context "when the viewing user is not the entry creator" do
+      context "when the viewing user is a liker" do
         it "shows 'You know the feeling'" do
           helper.like_status_for(entry, user)
             .should == "You know the feeling."
         end
       end
-
+      context "when the viewing user is not a liker" do
+        it "shows 'Another person knows the feeling'" do
+          helper.like_status_for(entry, FactoryGirl.create(:user))
+            .should == "Another person knows the feeling."
+        end
+      end
     end
     context "an entry with N likes" do
       let!(:entry) {FactoryGirl.create(:entry, user: FactoryGirl.create(:user), likes: 5)}
@@ -61,6 +66,30 @@ describe EntriesHelper do
           entry.like(user)
           helper.like_status_for(entry, user)
             .should == "You and <b>5</b> others know the feeling."
+        end
+      end
+    end
+  end
+
+  describe "#like_button_for" do
+    let!(:entry) {FactoryGirl.create(:entry, user: FactoryGirl.create(:user), likes: 5)}
+    context "when the viewing user is the entry creator" do
+      it "should not render a button" do
+        helper.like_button_for(entry, entry.user).should be_nil
+      end
+    end
+    context "when the viewing user is the not the entry creator" do
+      context "if he's not a liker of the entry" do
+        it "should render a 'been there' button" do
+          user = FactoryGirl.create(:user)
+          helper.like_button_for(entry, user).should match(/been there/)
+        end
+      end
+      context "if he's a liker of the entry" do
+        it "should render an 'un-been there' button" do
+          user = FactoryGirl.create(:user)
+          entry.like(user)
+          helper.like_button_for(entry, user).should match(/un-been there/)
         end
       end
     end
